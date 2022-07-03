af7f73d2276520f2a84dcb37a9858c51d2e1b836
Hub E Ali <58108501+Ali15700@users.noreply.github.com>
2022-07-03 23:02:20 +0500

    Update Menu.jsx

--- frontend/Menu.jsx
+++ frontend/Menu.jsx
@@ -8,7 +8,7 @@ export default function Menu({menuOpen ,setMenuOpen}) {
                     <a href="#intro">Home</a>
                 </li>
                 <li onClick={()=>setMenuOpen(!menuOpen)}>
-                    <a href="#portfolio">Top#5 Archietects</a>
+                    <a href="#archietect">Top#5 Archietects</a>
                 </li>
                 <li onClick={()=>setMenuOpen(!menuOpen)}>
                     <a href="#work">Trends</a>
